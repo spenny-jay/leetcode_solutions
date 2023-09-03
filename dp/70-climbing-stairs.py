10d58c3db114b5bac0e6cbb180b03227e33c3780
Spencer Josi <spenceraj@vt.edu>
2023-09-03 16:48:58 -0400

    Challenge 70: Climbing Stairs

---
+++ dp/70-climbing-stairs.py
@@ -0,0 +1,18 @@
+# LINK: https://leetcode.com/problems/climbing-stairs/description/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # values 1 - 3 will be n
+        if n <= 3: return n
+        # used to memoize results
+        dp = [0] * n
+        # initialize dp (dp[0] represents 1 stair, dp[1] represents 2 stairs)
+        dp[0] = 1
+        dp[1] = 2
+        # Every step count is a combination between the prior two
+        # (ex: combines results when the user move either 1 or 2 steps)
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        # return number of combinations when there are n steps
+        return dp[-1]
