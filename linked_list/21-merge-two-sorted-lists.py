df720005187899887f2304882557e94b58d73a3f
Spencer Josi <spenceraj@vt.edu>
2023-09-24 14:27:27 -0400

    Challenge 21: Merge Two Sorted Lists

---
+++ linked_list/21-merge-two-sorted-lists.py
@@ -0,0 +1,42 @@
+# LINK: https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # create a new node and make a pointer
+        # to maintain the start of the list
+        head = ListNode()
+        res = head
+
+        # traverse thru both ListNodes, increment the list
+        # with the lowest current value till we reach the end
+        # of one of them
+        while list1 and list2:
+            if list1.val < list2.val:
+                head.next = list1
+                list1 = list1.next
+            else:
+                head.next = list2
+                list2 = list2.next
+
+            head = head.next
+
+        # Add remaining values in list1
+        while list1:
+            head.next = list1
+            list1 = list1.next
+            head = head.next
+
+        # Add remaining values in list2
+        while list2:
+            head.next = list2
+            list2 = list2.next
+            head = head.next
+
+        # head.next will be the start of the list 
+        # res points to a empty ListNode
+        return res.next
