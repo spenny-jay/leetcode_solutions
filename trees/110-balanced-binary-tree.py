5a920846adf77f4d6a3984805e98c38ef4bbc593
Spencer Josi <spenceraj@vt.edu>
2023-09-27 00:53:48 -0400

    Challenge 110: Balanced Binary Tree

--- trees/110-balanced-binary-tree.py
+++ trees/110-balanced-binary-tree.py
@@ -11,18 +11,21 @@ class Solution:
         def dfs(root):
             if not root:
                 return 0
-
+            # use depth as returned from children subtrees (+ current node)
             left = dfs(root.left) + 1
             right = dfs(root.right) + 1
 
             diff = abs(left - right)
+            # if the left side is deeper than the right or vice versa
+            # invalidate all future paths with -1
             if diff > 2 or left == 0 or right == 0:
                 return -1
 
+            # otherwise, increment the depth!
             return 1 + max(left, right)
             
 
-
+        # -1 indicates that a diff > 2 was encountered in a path
         return dfs(root) != -1
         
 
